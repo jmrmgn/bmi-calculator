0b5551eabb37a3bfbd138c3716a3bd05698fd379
jomar <jomar.magno026@gmail.com>
2021-07-06 22:53:08 +0800

    Cleanup and finish BMI Calculator

--- bmi.go
+++ bmi.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/jmrmgn/bmi/info"
+)
 
 func main() {
-	fmt.Println("Test!")
+	fmt.Println(info.MainTitle)
+	fmt.Println(info.Separator)
+
+	fmt.Print(info.WeightPrompt)
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print(info.HeightPrompt)
+	heightInput, _ := reader.ReadString('\n')
+
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
+	bmi := weight / (height * height)
+
+	fmt.Printf("Your BMI: %.2f", bmi)
 }
